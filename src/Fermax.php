bc019f886577896bdc0d576b0efe029992b0df86
progOrbita <web.orbitadigital@gmail.com>
2023-06-01 09:43:44 +0200

    Update Fermax.php
    declare class attributes

--- src/Fermax.php
+++ src/Fermax.php
@@ -11,4 +11,11 @@ use Tools;
 
 class Fermax
 {
+    private $header;
+    private $key;
+    private $name;
+    private $lastError;
+    private $multilingualFields = ['name', 'description', 'description_short'];
+    private $gallery = ['img_main', 'img_1', 'img_2', 'img_3', 'img_4', 'img_5', 'img_6'];
+
 }
