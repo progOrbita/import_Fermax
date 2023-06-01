6e228dce657cc417e375a10baf6ffb0b4f99ddf0
progOrbita <web.orbitadigital@gmail.com>
2023-06-01 09:42:46 +0200

    Create Fermax.php

---
+++ src/Fermax.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Orbitadigital\Fermax;
+
+use Orbitadigital\Fermax\ReadFile;
+use Orbitadigital\Fermax\FermaxCategories;
+use Orbitadigital\Odfiles\JsonImporter;
+use Tools;
+
+class Fermax
+{
+}
