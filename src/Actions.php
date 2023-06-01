9b2d7a098d5e27cd97a655aa6e146b7a46438fbb
progOrbita <web.orbitadigital@gmail.com>
2023-06-01 09:54:04 +0200

    Create Actions.php

---
+++ src/Actions.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Orbitadigital\Fermax;
+
+use Orbitadigital\Fermax\FermaxTranslate;
+use Product;
+use StockAvailable;
+use Image;
+use AdminImportControllerCore;
+
+class Actions extends AdminImportControllerCore
+}
