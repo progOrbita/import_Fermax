18a573a81db715353eea4fa3f5a26f85e860c634
progOrbita <web.orbitadigital@gmail.com>
2023-06-01 09:56:17 +0200

    Update Actions.php
    add function update() to update a ps product

--- src/Actions.php
+++ src/Actions.php
@@ -48,4 +48,60 @@ class Actions extends AdminImportControllerCore
         return 'Descatalogado correctamente';
     }
 
+    /**
+     * update ps product data 
+     * 
+     * @param int $idProduct
+     * @param array $productData
+     * 
+     * @return string
+     */
+    public static function update(int $idProduct, array $productData, bool $write = false): string
+    {
+        global $cat;
+        $changes = false;
+        $product = new Product($idProduct);
+        $product->loadStockData();
+        if ($productData['quantity'] != $product->quantity) {
+            $changes[] = 'quantity (' . $product->quantity . '->' . $productData['quantity'] . ')';
+            $write && StockAvailable::setQuantity($idProduct, 0, $productData['quantity']);
+            $product->quantity = $productData['quantity'];
+        }
+
+        if ($productData['id_supplier'] > 0 && $productData['supplier_reference']) {
+            $product->addSupplierReference($productData['id_supplier'], 0, $productData['supplier_reference']);
+        }
+
+        if ($product->id_supplier != $productData['id_supplier']) {
+            $changes[] = 'id_supplier (' . $product->id_supplier . '->' . $productData['id_supplier'] . ')';
+        }
+
+        if ($product->id_category_default == 10) {
+            if ($productData['id_category_default'] < 1) {
+                return 'No se ha podido recatalogar por problemas en la relacion de las categorias';
+            }
+
+            $product->available_for_order = true;
+            $product->visibility = 'both';
+            $product->show_price = true;
+            $product->id_category_default = $productData['id_category_default'];
+            $product->updateCategories($cat->getCacheCategories($productData['id_category_default']));
+            $changes[] = 'Producto a recatalogar';
+        }
+
+        if (!$changes) {
+            return 'Sin cambios';
+        }
+
+        if (!$write) {
+            return 'Datos a actualizar: ' . implode(',', $changes);
+        }
+
+        if (!$product->update()) {
+            return 'Error con la actualizacion';
+        }
+
+        return 'Actualizado: ' . implode(',', $changes);
+    }
+
 }
