dc9071c98fbdb9afff84dd78fc7c24b48e44adb6
progOrbita <web.orbitadigital@gmail.com>
2023-06-01 09:59:39 +0200

    Update index.php
    set xlsx headers

--- index.php
+++ index.php
@@ -17,3 +17,59 @@ use Throwable;
 use Categories;
 use Orbitadigital\Fermax\FermaxTranslate;
 
+$header = [
+    "COD",
+    "REFERENCIA",
+    "NOMBRE",
+    "categoria",
+    "sub categoria",
+    "DESCRIPCIÓN",
+    "DETALLES TÉCNICOS",
+    "IMAGEN PRINCIPAL",
+    "VISTA LATERAL",
+    "VISTA TRASERA",
+    "IMAGEN DE COTAS",
+    "ESQUEMA DE INSTALACIÓN UNIFILAR",
+    "ESQUEMA DE CABLEADO",
+    "OTRAS IMÁGENES",
+    "EAN",
+    "ALTO Embalaje (mm)",
+    "ANCHO Embalaje (mm)",
+    "PROFUNDIDAD Embalaje (mm)",
+    "Peso producto embalado (kg)",
+    "DECLARACION DE CONFORMIDAD",
+    "FICHA TÉCNICA",
+    "PVPR",
+    "PVP",
+    "HS Codes",
+    "NFAM"
+];
+
+$new_header = [
+    "reference",
+    "supplier_reference",
+    "name",
+    "category",
+    "subcategory",
+    "description_short",
+    "description",
+    "img_main",
+    "img_1",
+    "img_2",
+    "img_3",
+    "img_4",
+    "img_5",
+    "img_6",
+    "ean13",
+    "height",
+    "width",
+    "depth",
+    "weight",
+    "DECLARACION DE CONFORMIDAD",
+    "FICHA TÉCNICA",
+    "minimun_price",
+    "price",
+    "HS Codes",
+    "NFAM"
+];
+
