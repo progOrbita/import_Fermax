e4187633465dee6d59bfeec0f42f8a30fd25196a
progOrbita <web.orbitadigital@gmail.com>
2023-06-01 09:47:56 +0200

    Update FermaxCategories.php
    declare class attributes

--- src/FermaxCategories.php
+++ src/FermaxCategories.php
@@ -8,4 +8,9 @@ use Orbitadigital\Odfiles\JsonImporter;
 
 class FermaxCategories
 {
+    private $data = [];
+    private $name = _PS_CORE_DIR_ . '/import/Fermax/data/FermaxJsonCategories.json';
+    private $lastError = '';
+    private $originalData = [];
+
 }
